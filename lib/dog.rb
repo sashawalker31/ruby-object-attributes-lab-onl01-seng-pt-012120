662a28a4cb8be3c8f0ca462572674e5bd107b0aa
sasha walker <sashawalker31@gmail.com>
2020-02-08 02:48:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
-class Dog 
-  def dog=(dogs_name) 
+class Dog
+  
+  def name=(dogs_name)
     @name = dogs_name
-  end 
-  
-  def dog 
-    @name 
-  end 
-
-  def dog_breed=(dogs_breed)
-    @breed = breed
-  end 
-  
-  def dog_breed
-    @breed
   end
-end 
+  
+  def name
+    @name 
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end 
+end
