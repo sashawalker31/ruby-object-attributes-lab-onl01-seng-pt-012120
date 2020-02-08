df1abcacd8153d2b0bd7949f2b56679e3961ad0a
sasha walker <sashawalker31@gmail.com>
2020-02-08 02:39:40 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def person 
+    @name = name 
+  end 
+  
+  def person 
+    @name 
+  end 
+  
+  def person_job
+    @job 
+  end 
+  
+  def person_job
+    @job = job 
+  end 
+end
